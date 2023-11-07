1b7e2297c54569f0bcef7fe5390d56cf8b636b8e
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-11-07 11:33:12 -0300

    solve-me-first

---
+++ src/hacker-rank/easy/solve-me-first.js
@@ -0,0 +1,16 @@
+/*
+Solve Me First: Um problema simples de algoritmos que pede para calcular
+a soma de dois inteiros. O problema tem exemplos de entrada e saída,
+uma descrição da função a ser completada, e restrições dos parâmetros.
+https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true
+*/
+
+function solveMeFirst(a, b) {
+  const firstValue = a;
+  const secondValue = b;
+
+  const result = firstValue + secondValue;
+  return result;
+}
+
+solveMeFirst(2, 3);
