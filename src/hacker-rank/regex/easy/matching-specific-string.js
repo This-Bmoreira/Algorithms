1a170269fe50da6e1dcba5ab7dab3e6210bb063d
This-Bmoreira <contatobrunosmoreira@gmail.com>
2023-11-10 09:27:03 -0300

    Move the file to a new folder

--- src/hacker-rank/regex/easy/matching-specific-string.js
+++ src/hacker-rank/regex/easy/matching-specific-string.js
@@ -12,6 +12,7 @@ Desafio de regex: A página propõe um desafio de regex
 que não requer escrever código, apenas preencher o padrão
 regex em um espaço em branco. O desafio é encontrar a
 string “hackerrank” em uma string de teste, respeitando a sensibilidade a maiúsculas e minúsculas.
+https://www.hackerrank.com/challenges/matching-specific-string/problem?isFullScreen=true
 */
 
 const text = 'The hackerrank team is on a mission to flatten the world by restructuring the DNA of every company on the planet. We rank programmers based on their coding skills, helping companies source great programmers and reduce the time to hire. As a result, we are revolutionizing the way companies discover and evaluate talented engineers. The hackerrank platform is the destination for the best engineers to hone their skills and companies to find top engineers. hackerrank is hackerrank not HackerRank. so please use hackerrank always and not hackerrrank hackerrank 2 hackerrank 3 hackerrank 4 hackerrank 5 not Hackerrank 6 is hackerrank';
